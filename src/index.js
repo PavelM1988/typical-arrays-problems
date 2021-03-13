249ce0901abdcc69ef7f39d4a21eee8242995169
PavelM1988 <79479113+PavelM1988@users.noreply.github.com>
2021-03-13 15:14:26 +0300

    init: update file index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,58 @@
-
+// Returns min in array
 exports.min = function min (array) {
-  return 0;
+
+	
+	if (array == undefined)
+		return 0;
+	if (array.length == 0)
+		return 0;
+
+	var min;
+
+	min = array[0];
+
+	for (var i = 1; i < array.length; ++i) {
+		if (array[i] < min)
+			min = array[i];
+	}
+
+	return min;
 }
+
+// Returns max in array
 
 exports.max = function max (array) {
-  return 0;
+
+	if (array == undefined)
+		return 0;
+	if (array.length == 0)
+		return 0;
+
+	var max;
+
+	max = array[0];
+
+	for (var i = 1; i < array.length; ++i) {
+		if (array[i] > max)
+			max = array[i];
+	}
+
+	return max;
 }
 
+// Returns avarage in array
 exports.avg = function avg (array) {
-  return 0;
+
+	if (array == undefined)
+		return 0;
+	if (array.length == 0)
+		return 0;
+	
+	var sum = 0;
+
+	for (var i = 0; i < array.length; ++i)
+		sum += array[i];
+
+	return sum / array.length;
+
 }
